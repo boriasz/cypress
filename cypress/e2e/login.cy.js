28c7d73d837ca87619dac53e6b916adf7ef0ba21
Borys Stefański <borys.stefanski@gmail.com>
2025-02-20 21:51:55 +0100

    Add login tests

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,14 +1,22 @@
 describe('Login tests', () => {
-    it('Verify', () => {
+    it('Verify ', () => {
       cy.visit('https://the-internet.herokuapp.com/login');
       cy.get('h2').should('contain', 'Login Page');
     });
-    it('Open login and try to login using wrong password', () => {
+    it('Open login and try to login using correct credentials', () => {
         cy.visit('https://the-internet.herokuapp.com/login');
         cy.get('#username').type("tomsmith");
         cy.get('#password').type("SuperSecretPassword!");
         cy.get('.radius').click();
         cy.location('pathname').should('eq', '/secure');
+      });
+      it('Open login and try to login using wrong password', () => {
+        const randomString = Math.random().toString(36).substring(2, 10);
+        cy.visit('https://the-internet.herokuapp.com/login');
+        cy.get('#username').type("tomsmith");
+        cy.get('#password').type(randomString);
+        cy.get('.radius').click();
+        cy.contains('Your password is invalid!')
       });
   });
   
