d60cf72e8ed500b63b670c9acf9490a6255137a3
Borys Stefański <borys.stefanski@gmail.com>
2025-02-21 13:57:18 +0100

    Add login function in commands.js

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -6,39 +6,29 @@ describe('Login tests', () => {
       cy.get('h2').should('contain', 'Login Page');
     });
     it('Open login and try to login using correct credentials', () => {
-        cy.get('#username').type("tomsmith");
-        cy.get('#password').type("SuperSecretPassword!");
-        cy.get('.radius').click();
+        cy.login('tomsmith', 'SuperSecretPassword!')
         cy.location('pathname').should('eq', '/secure');
       });
       it('Open login and try to login using wrong password', () => {
         const randomString = Math.random().toString(36).substring(2, 10);
-        cy.get('#username').type("tomsmith");
-        cy.get('#password').type(randomString);
-        cy.get('.radius').click();
+        cy.login('tomsmith', randomString)
         cy.contains('Your password is invalid!')
       });
       it('Open login and try to login using wrong username', () => {
         const randomString = Math.random().toString(36).substring(2, 10);
-        cy.get('#username').type(randomString);
-        cy.get('#password').type("SuperSecretPassword!");
-        cy.get('.radius').click();
+        cy.login(randomString, 'SuperSecretPassword!')
         cy.contains('Your username is invalid!')
       });
       it('Open login and try to login using empty username', () => {
-        cy.get('#password').type("SuperSecretPassword!");
-        cy.get('.radius').click();
+        cy.login('{selectall}{backspace}', 'SuperSecretPassword!')
         cy.contains('Your username is invalid!')
       });
       it('Open login and try to login using empty passwrd', () => {
-        cy.get('#username').type("tomsmith");
-        cy.get('.radius').click();
+        cy.login('tomsmith', '{selectall}{backspace}')
         cy.contains('Your password is invalid!')
       });
       it('Open login, try to login using correct credentials and logout', () => {
-        cy.get('#username').type("tomsmith");
-        cy.get('#password').type("SuperSecretPassword!");
-        cy.get('.radius').click();
+        cy.login('tomsmith', 'SuperSecretPassword!')
         cy.location('pathname').should('eq', '/secure');
         cy.get('.button.secondary.radius').click();
         cy.location('pathname').should('eq', '/login');
