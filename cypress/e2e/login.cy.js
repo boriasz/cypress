e25ed350d56d685edcadb71128a9b4ddf6722b59
Borys Stefański <borys.stefanski@gmail.com>
2025-02-21 07:56:23 +0100

    Add logout test

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,5 +1,5 @@
 describe('Login tests', () => {
-    it('Verify ', () => {
+    it('Verify login page', () => {
       cy.visit('https://the-internet.herokuapp.com/login');
       cy.get('h2').should('contain', 'Login Page');
     });
@@ -17,6 +17,35 @@ describe('Login tests', () => {
         cy.get('#password').type(randomString);
         cy.get('.radius').click();
         cy.contains('Your password is invalid!')
+      });
+      it('Open login and try to login using wrong username', () => {
+        const randomString = Math.random().toString(36).substring(2, 10);
+        cy.visit('https://the-internet.herokuapp.com/login');
+        cy.get('#username').type(randomString);
+        cy.get('#password').type("SuperSecretPassword!");
+        cy.get('.radius').click();
+        cy.contains('Your username is invalid!')
+      });
+      it('Open login and try to login using empty username', () => {
+        cy.visit('https://the-internet.herokuapp.com/login');
+        cy.get('#password').type("SuperSecretPassword!");
+        cy.get('.radius').click();
+        cy.contains('Your username is invalid!')
+      });
+      it('Open login and try to login using empty passwrd', () => {
+        cy.visit('https://the-internet.herokuapp.com/login');
+        cy.get('#username').type("tomsmith");
+        cy.get('.radius').click();
+        cy.contains('Your password is invalid!')
+      });
+      it('Open login, try to login using correct credentials and logout', () => {
+        cy.visit('https://the-internet.herokuapp.com/login');
+        cy.get('#username').type("tomsmith");
+        cy.get('#password').type("SuperSecretPassword!");
+        cy.get('.radius').click();
+        cy.location('pathname').should('eq', '/secure');
+        cy.get('.button.secondary.radius').click();
+        cy.location('pathname').should('eq', '/login');
       });
   });
   
